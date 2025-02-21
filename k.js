37394c269ea42e19e453f4ed6c6e9de6eac14bfc
Valentin1599 <97vesi@gmail.com>
2025-02-21 13:31:56 +0200

    'vvvvv

---
+++ k.js
@@ -0,0 +1,41 @@
+let yearNumber =2
+switch (yearNumber) {
+    case 1:
+    console.log("qnuari");
+    break
+    case 2:
+        console.log("fevruari");
+    break
+    case 3:
+        console.log("mart");
+    break
+    case 4:
+        console.log("april");
+    break
+    case 5:
+        console.log("mai");
+    break
+     case 6:
+            console.log("uni");
+    break
+        case 7:
+            console.log("uli");
+    break
+        case 8:
+            console.log("avgust");
+    break
+        case 9:
+            console.log("septemvri");
+    break
+        case 10:
+            console.log("oktomvri");
+    break
+        case 11:
+            console.log("noemvri");
+    break
+        case 12:
+            console.log("dekemvri");
+        break
+            
+    
+}
---
+++ programa.js
@@ -0,0 +1,26 @@
+let x=100
+let y=88
+let s="-"
+if (s=="/")
+{
+    let v=x/y
+    console.log(v)
+}
+
+if (s=="+")
+{
+    let v=x+y
+    console.log(v)
+}
+
+if (s=="-")
+{
+    let v=x-y
+    console.log(v)
+}
+    
+if (s=="*")
+{
+    let v=x*y
+    console.log(v)
+}
